caf4a403c602c85e1a9e1c44cd3cab24ed2d1254
dnevar <32317967+dnevar@users.noreply.github.com>
2025-03-16 18:12:44 -0500

    comment hecho en github para probar la funcion fetch en git

--- hello3.py
+++ hello3.py
@@ -12,3 +12,5 @@ if __name__ == "__main__":
 print("Hello, World!")
 print("Commit despues de cambiar mi correo por el de la universidad")
 print("El segundo commit que voy a subir a mi repositorio remoto")
+
+print("Voy a probar la funcion fetch")
