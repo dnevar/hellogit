3b51f4e7dd852df9469e178095b7d078ec53c373
dnevar <dnevar@unal.edu.co>
2025-03-16 18:09:58 -0500

    poner todo al día

--- hellogit.py
+++ hellogit.py
@@ -2,4 +2,4 @@ print("hello world")
 print("Mi tercer commit")
 print("Commit despues de cambiar mi correo por el de la universidad")
 
-print("El segundo commit que voy a subir a mi repositorio remoto")
+print("El segundo commit que voy a subir a mi repositorio remoto")q
